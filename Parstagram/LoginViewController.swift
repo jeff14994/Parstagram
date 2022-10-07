ab5b32f8aeaf318e8274658d4e798dcbe4bb1a00
jeff14994 <jeff14994@gmail.com>
2022-10-07 03:14:18 -0700

    Link the storyboard to the elements

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
